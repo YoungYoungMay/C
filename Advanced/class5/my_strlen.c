edc92e4e4520d7bdaefe9bf3a361b233665844bc
YoungMay_ <102332172@qq.com>
2018-08-19 13:47:05 +0800

    模拟实现strlen函数

--- Advanced/class5/my_strlen.c
+++ Advanced/class5/my_strlen.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <string.h>
 
+//方法1->计数器方式
 size_t my_strlen(const char* str)
 {
     size_t len = 0;
@@ -16,10 +17,34 @@ size_t my_strlen(const char* str)
     return len;
 }
 
+//方法2->不创建临时计数器
+size_t my_strlen1(const char* str)
+{
+    if(*str == '\0')//空串
+        return 0;
+    return 1 + my_strlen1(str+1);
+}
+
+//方法3->指针方式
+size_t my_strlen2(const char* str)
+{
+    char* p = (char*)str;
+    while(*p != '\0')
+        p++;
+    return p-str;
+}
+
 int main()
 {
     char* str = "Hello, this is YoungMay!";
     size_t size = my_strlen(str);
     printf("expected is %d, actual is %d\n", strlen(str), size);
+ 
+    size_t size1 = my_strlen1(str);
+    printf("expected is %d, actual is %d\n", strlen(str), size1);
+
+    size_t size2 = my_strlen2(str);
+    printf("expected is %d, actual is %d\n", strlen(str), size2);
+
     return 0;
 }
